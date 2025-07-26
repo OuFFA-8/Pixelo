4eedec93e248a73158cdc65946b4118b466fa167
OuFFA-8 <ouffaalaa26@gmail.com>
2025-07-26 14:22:52 +0300

    n

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Routes } from '@angular/router';
+
 
 export const serverRoutes: ServerRoute[] = [
-  {
+   {
     path: 'products/:id',
-    renderMode: RenderMode.Prerender 
+    renderMode: RenderMode.Server, 
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Server, 
   },
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
-];  
+];
+
